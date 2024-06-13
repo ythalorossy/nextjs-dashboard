3996e4732faf4086e4814ac709cdfb43edc8f87d
Ythalo Saldanha <ythalorossy@gmail.com>
2024-06-12 23:40:11 -0400

    Added  multiple origins

--- next.config.js
+++ next.config.js
@@ -5,12 +5,20 @@ const nextConfig = {
             {
                 // matching all API routes
                 source: "/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
+                headers: (req) => {
+                    const allowedOrigins = ['http://zwcs0w4.69.138.6.30.sslip.io:3000', 'http://localhost:3000', '*'];
+                    const origin = req.headers.origin;
+                    if (allowedOrigins.includes(origin)) {
+                        return [
+                            { key: "Access-Control-Allow-Credentials", value: "true" },
+                            { key: "Access-Control-Allow-Origin", value: origin },
+                            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                        ];
+                    } else {
+                        return []; // return empty array if the origin is not allowed
+                    }
+                }
             }
         ]
     }
